f1f81a2e97b8eafed938ad033fbbb007607f0d1a
Miguel Savignano <migue.masx@gmail.com>
2017-09-09 20:43:08 +0200

    provider definition open a file in tab

--- src/PartialNameDefinitionProvider.ts
+++ src/PartialNameDefinitionProvider.ts
@@ -1,9 +1,9 @@
 import openFileInTap from './openFileInTap'
+import { ProviderResult } from 'vscode'
 
 export default class PartialNameDefinitionProvider{
-  provideDefinition(document: TextDocument, position: Position, token: CancellationToken){
-    console.log("title.html.erb")
-    debugger;
-    // await openFileInTap("source/title.html.erb")
+  async provideDefinition(document: TextDocument, position: Position, token: CancellationToken){
+    const response = await openFileInTap("source/title.html.erb")
+    return new ProviderResult
   }
 }
