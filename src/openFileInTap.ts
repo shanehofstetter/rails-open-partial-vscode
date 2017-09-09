1c8049d8d52a8a5f737bf9b676d67647628ab988
Miguel Savignano <migue.masx@gmail.com>
2017-09-10 01:59:51 +0200

    fix bug open in tap

--- src/openFileInTap.ts
+++ src/openFileInTap.ts
@@ -8,10 +8,10 @@ const { rootPath }  = workspace
 async function openFileInTap(fileName){
   try{
     const textDocument = await workspace.openTextDocument(`${rootPath}/${fileName}`)
+    const editor = window.showTextDocument(textDocument)
   } catch(error) {
     throw new Error('Could not open file!')
   }
-  const editor = window.showTextDocument(textDocument)
 }
 
 export default openFileInTap
