83dd01cb3741a5cb7c743a0780ded041d35ccfb7
Miguel Savignano <migue.masx@gmail.com>
2017-09-09 23:03:30 +0200

    test ok

--- src/PartialNameDefinitionProvider.ts
+++ src/PartialNameDefinitionProvider.ts
@@ -1,9 +1,28 @@
 import openFileInTap from './openFileInTap'
-import { ProviderResult } from 'vscode'
+import {
+  ProviderResult,
+  TextDocument,
+  Position,
+  CancellationToken,
+  DefinitionProvider,
+  Location,
+  Uri
+ } from 'vscode'
 
-export default class PartialNameDefinitionProvider{
-  async provideDefinition(document: TextDocument, position: Position, token: CancellationToken){
-    const response = await openFileInTap("source/title.html.erb")
-    return new ProviderResult
+export default class PartialNameDefinitionProvider implements DefinitionProvider{
+  async provideDefinition(document: TextDocument, position: Position, token: CancellationToken) {
+    const partialName = this.getPartiaName(document, position)
+    const response = await openFileInTap(partialName)
+    return undefined;
+  }
+
+  getPartiaName(document: TextDocument, position: Position){
+    return this.getPartialNameFromLine(
+      document.lineAt(position.line).text
+    )
+  }
+
+  getPartialNameFromLine(line_text){
+    return "source/title.html.erb"
   }
 }
