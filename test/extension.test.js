b8584fb852d5168bc7932b2aad9ba79b6724dee6
Miguel Savignano <migue.masx@gmail.com>
2017-09-10 01:21:43 +0200

    test get partial name from line

--- test/extension.test.js
+++ test/extension.test.js
@@ -1,16 +1,33 @@
 var assert = require('assert');
 
 function getPartialNameFromLine(line_text){
-  return "title"
+  var line_text = line_text.split(" ").filter(function(i){ return i != "" }).join(" ")
+  var after_partial = line_text.split(/partial\(|partial\ /)[1]
+  var first_argument = after_partial.split(/\ |\,/)[0]
+  var partial_name = first_argument.replace(/\"|\'|\)|\:/g, "")
+  return partial_name
 }
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
+
+describe('getPartialNameFromLine', function() {
+  it('with <%= partial "blocks/header" %>', function() {
+    assert.equal(
+      getPartialNameFromLine('<%= partial "blocks/header" %>'),
+      "blocks/header"
+    )
+  });
+
+  [
+    '<%= partial "title" %>',
+    '<%= partial("title") %>',
+    '<%= partial :"title" %>',
+    '<%= partial(:"title") %>',
+    '<%= partial  "title" %>'
+  ].forEach(function(example){
+    it('with ' + example, function() {
       assert.equal(
-        getPartialNameFromLine("<%= partial 'title' %>"),
+        getPartialNameFromLine(example),
         "title"
       )
-      assert.equal(1+1, 2);
-    });
-  });
-});
+    })
+  })
+})
