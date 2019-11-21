6a637d97ce295179a653ff7e8acba0db1b200e56
yamitake <take.yapr@gmail.com>
2019-11-21 17:40:00 +0900

    slim compatible

--- src/extension.ts
+++ src/extension.ts
@@ -6,8 +6,10 @@ export function activate(context: vscode.ExtensionContext) {
   console.log('activated extension "rails-open-partial"');
   const HAML = { language: 'haml', scheme: 'file' };
   const ERB = { language: 'erb', scheme: 'file' };
+  const SLIM = { language: 'slim', scheme: 'file' };
   context.subscriptions.push(vscode.languages.registerDefinitionProvider(HAML, new PartialNameDefinitionProvider));
   context.subscriptions.push(vscode.languages.registerDefinitionProvider(ERB, new PartialNameDefinitionProvider));
+  context.subscriptions.push(vscode.languages.registerDefinitionProvider(SLIM, new PartialNameDefinitionProvider));
 }
 
 export function deactivate() {
